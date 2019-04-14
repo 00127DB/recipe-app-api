afa8fef1ae20438c1a521e021585d8759e0fd94f
daniel00127 <daniel.lamas@protonmail.com>
2019-04-14 01:12:02 -0700

    Added list ingrdients feature

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write('Waiting for db...')
         db_conn = None
         while not db_conn:
             try:
@@ -17,4 +17,4 @@ class Command(BaseCommand):
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write('Database is good...')
